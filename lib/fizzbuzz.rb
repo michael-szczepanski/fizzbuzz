4ad0a8799b0dc6786db305d1cc1aead99e0549cb
Christie <christiegriffiths@outlook.com>
2023-04-08 11:20:10 +0100

    irb testing

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,17 @@
 def fizzbuzz(number)
-  'fizz'
+  if number % 3 == 0 && number % 5 == 0
+  "fizzbuzz"
+  elsif number % 3 == 0 
+    "fizz"
+  elsif number % 5 == 0
+    "buzz"
+  else 
+    number 
+  end
 end
+
+def hundredclub 
+  (1..10).each do |i| 
+    puts fizzbuzz(i)
+    end
+end 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,9 +4,6 @@ describe 'fizzbuzz' do
     it 'returns "fizz" when passed 3' do 
         expect(fizzbuzz(3)).to eq 'fizz'
     end
-<<<<<<< HEAD
-end 
-=======
     
     it 'returns "buzz" when passed 5' do
         expect(fizzbuzz(5)).to eq 'buzz'
@@ -20,4 +17,3 @@ end
         expect(fizzbuzz(15)).to eq 'fizzbuzz'
     end
 end
->>>>>>> f27f0f2 (new test cases added)
