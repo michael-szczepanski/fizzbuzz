a8b237fe54a10c1e721b3d0c11ef459f26313ef8
Michael Szczepanski <m.szczepanski90@gmail.com>
2023-04-08 09:48:13 +0000

    new test cases added

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,20 @@ describe 'fizzbuzz' do
     it 'returns "fizz" when passed 3' do 
         expect(fizzbuzz(3)).to eq 'fizz'
     end
+<<<<<<< HEAD
 end 
+=======
+    
+    it 'returns "buzz" when passed 5' do
+        expect(fizzbuzz(5)).to eq 'buzz'
+    end
+    
+    it 'returns 7 when passed 7' do
+        expect(fizzbuzz(7)).to eq 7
+    end
+    
+    it 'returns "fizzbuzz" when passed 15' do
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+end
+>>>>>>> f27f0f2 (new test cases added)
